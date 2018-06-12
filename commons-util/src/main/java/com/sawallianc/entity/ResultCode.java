b199a19e7ee10d041a8574b3290c28d1896d12b5
sawallianc <1223553705@qq.com>
2018-06-12 23:46:17 +0800

    申诉状态码

--- commons-util/src/main/java/com/sawallianc/entity/ResultCode.java
+++ commons-util/src/main/java/com/sawallianc/entity/ResultCode.java
@@ -21,6 +21,7 @@ public class ResultCode {
 	public static final ResultCode ALIPAY_PAY_FAILURE = getResultCode(-14,"支付宝支付失败");
 	public static final ResultCode ALIPAY_PRE_ORDER_FAILURE = getResultCode(-15,"支付宝预下单失败");
 	public static final ResultCode GOODS_DOWN = getResultCode(-16,"所选商品已下架，请刷新页面重新购买");
+	public static final ResultCode ALREADY_APPEALED = getResultCode(-17,"已有申诉正在处理中，请勿重复提交");
     /**
      * 对于逃单的用户
      */
